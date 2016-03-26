ae24ca44ec6e8ef3425335ffaae235a28f1b78f7
mengarena <mengrufeng@gmail.com>
2016-03-25 21:26:45 -0400

    Added files via upload

---
+++ com/leet/TwoSumIIIDataStructureDesign.java
@@ -0,0 +1,168 @@
+package com.leet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+//Design and implement a TwoSum class. It should support the following operations: add and find.
+//
+//add - Add the number to an internal data structure.
+//find - Find if there exists any pair of numbers which sum is equal to the value.
+//
+//For example,
+//add(1); add(3); add(5);
+//find(4) -> true
+//find(7) -> false
+
+public class TwoSumIIIDataStructureDesign {
+
+	public TwoSumIIIDataStructureDesign() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	//Accepted: 90%
+	private Map<Integer, Integer> hmFreq = new HashMap<Integer, Integer>();
+	private List<Integer> lstNumbers = new ArrayList<Integer>();
+	
+    // Add the number to an internal data structure.
+	public void add(int number) {		
+		if (hmFreq.containsKey(number)) {
+			//hmFreq.put(number, hmFreq.get(number)+1);   //Similar performance
+			hmFreq.put(number, 2);  //Don't need to same two many duplicate values, only two is enough
+		} else {
+			hmFreq.put(number, 1);
+			lstNumbers.add(number);
+		}
+	}
+
+    // Find if there exists any pair of numbers which sum is equal to the value.
+	public boolean find(int value) {	    
+        for (int i=0; i<lstNumbers.size(); i++) {
+        	int num = lstNumbers.get(i);
+        	int nOther = value - num;
+        	
+        	if (nOther == num && hmFreq.get(num) > 1) return true;
+        	if (nOther != num && hmFreq.containsKey(nOther)) return true;
+        }
+	    
+	    return false;
+	}
+
+	
+	
+
+	
+	
+	
+	
+	
+	
+	//AC: slow 3%
+	Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
+	
+    // Add the number to an internal data structure.
+	public void addA(int number) {
+		if (hm.containsKey(number)) {
+			hm.put(number, 2);   //Don't need to same two many duplicate values, only two is enough
+		} else {
+			hm.put(number, 1);
+		}
+	}
+
+    // Find if there exists any pair of numbers which sum is equal to the value.
+	public boolean findA(int value) {
+	    Set<Integer> setKeys = hm.keySet();
+	    
+	    if (setKeys == null || setKeys.isEmpty()) return false;
+	    
+	    for (Integer nKey:setKeys) {
+	    	int nOther = value - nKey;
+	    	if (nOther == nKey && hm.get(nKey) > 1) return true;
+	    	
+	    	if (hm.containsKey(nOther) && nOther != nKey) return true;
+	    }
+	    
+	    return false;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/*	
+	
+	//Exceeded time limit
+	Set<Integer> setRawNumbers = new HashSet<Integer>();
+	Set<Integer> setSums = new HashSet<Integer>();	
+	
+    // Add the number to an internal data structure.
+	public void addB(int number) {
+	    if (setRawNumbers.contains(number)) return;
+	    int nSum = 0;
+	    
+	    for (Integer num:setRawNumbers) {
+	        nSum = num + number;
+	        if (!setSums.contains(nSum)) setSums.add(nSum);
+	    }
+	    
+	    setRawNumbers.add(number);
+	}
+
+	
+    // Find if there exists any pair of numbers which sum is equal to the value.
+	public boolean findB(int value) {
+        return setSums.contains(value);
+	}
+	
+	
+	
+	
+	//Naive solution, Exceed time limit	
+	Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
+		
+    // Add the number to an internal data structure.
+	public void addA(int number) {
+	    hm.put(number, hm.containsKey(number) ? hm.get(number)+1: 1);
+	}
+
+    // Find if there exists any pair of numbers which sum is equal to the value.
+	public boolean findA(int value) {
+	    Set<Integer> setKeys = hm.keySet();
+	    
+	    if (setKeys == null || setKeys.isEmpty()) return false;
+	    
+	    for (Integer nKey:setKeys) {
+	    	int nOther = value - nKey;
+	    	if (nOther == nKey) {
+	    		if (hm.get(nKey) > 1) return true;
+	    		return false;
+	    	}
+	    	
+	    	if (hm.containsKey(value - nKey)) return true;
+	    }
+	    
+	    return false;
+	}
+	
+*/	
+	// Your TwoSum object will be instantiated and called as such:
+	// TwoSum twoSum = new TwoSum();
+	// twoSum.add(number);
+	// twoSum.find(value);
+}
---
+++ com/leet/UniqueWordAbbreviation.java
@@ -0,0 +1,98 @@
+package com.leet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+//An abbreviation of a word follows the form <first letter><number><last letter>. 
+//Below are some examples of word abbreviations:
+//
+//a) it                      --> it    (no abbreviation)
+//
+//     1
+//b) d|o|g                   --> d1g
+//
+//              1    1  1
+//     1---5----0----5--8
+//c) i|nternationalizatio|n  --> i18n
+//
+//              1
+//     1---5----0
+//d) l|ocalizatio|n          --> l10n
+//
+//Assume you have a dictionary and given a word, find whether its abbreviation is unique in the dictionary. 
+//A word's abbreviation is unique if no other word from the dictionary has the same abbreviation.
+//
+//Example: 
+//Given dictionary = [ "deer", "door", "cake", "card" ]
+//
+//isUnique("dear") -> false
+//isUnique("cart") -> true
+//isUnique("cane") -> false
+//isUnique("make") -> true
+
+
+public class UniqueWordAbbreviation {
+
+	public UniqueWordAbbreviation() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	//Pay attention to the description of the question
+	//***A word's abbreviation is unique if no other word from the dictionary has the same abbreviation***
+	//If the dictionary has a word "hello",  if we use isUnique to test a word "hello", the two "hello" will be considered the same, so isUnique("hello") = true (if no other h2o words exist)
+	//While in the example given in the question, "cane" has same abbr as "cake", but they are different words
+    private Map<String, List<String>> hmDict = new HashMap<String, List<String>>();
+
+    public void ValidWordAbbr(String[] dictionary) {   //Original version for the question should be (constructor): public ValidWordAbbr(String[] dictionary)
+        if (dictionary == null || dictionary.length == 0) return;
+        
+        for (int i=0; i<dictionary.length; i++) {
+        	String sAbbr = getAbbr(dictionary[i]);
+        	
+            if (hmDict.containsKey(sAbbr)) {
+                if (!hmDict.get(sAbbr).contains(dictionary[i])) hmDict.get(sAbbr).add(dictionary[i]);
+            } else {
+                List<String> lstValue = new ArrayList<String>();
+                lstValue.add(dictionary[i]);
+                hmDict.put(sAbbr, lstValue);
+            }
+        	
+        }
+    }
+
+    private String getAbbr(String s) {
+    	int n = s.length();
+    	if (n <= 2) return s;
+    	
+    	return s.charAt(0) + "" + (n-2) + s.charAt(n-1);
+    }
+    
+    
+    public boolean isUnique(String word) {
+        if (word == null) return false;
+        if (hmDict.size() == 0) return true;
+        
+        String sAbbr = getAbbr(word);
+        
+        if (!hmDict.containsKey(sAbbr)) return true;
+        
+    	if (hmDict.get(sAbbr).size() > 2) return false;
+    	
+    	if (hmDict.get(sAbbr).size() == 1 && word.equals(hmDict.get(sAbbr).get(0))) return true;
+    	
+    	return false;
+    }
+    
+    
+}
+
+
+// Your ValidWordAbbr object will be instantiated and called as such:
+// ValidWordAbbr vwa = new ValidWordAbbr(dictionary);
+// vwa.isUnique("Word");
+// vwa.isUnique("anotherWord");
