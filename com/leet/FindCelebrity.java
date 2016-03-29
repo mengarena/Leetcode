bda1b2e624ebf28234534f6c70d2d056df3d7e0c
mengarena <mengrufeng@gmail.com>
2016-03-29 00:17:05 -0400

    Added files via upload

---
+++ com/leet/FindCelebrity.java
@@ -0,0 +1,121 @@
+package com.leet;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there may exist one celebrity. 
+//The definition of a celebrity is that all the other n - 1 people know him/her but he/she does not know any of them.
+//
+//Now you want to find out who the celebrity is or verify that there is not one. 
+//The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?" to get information of whether A knows B. 
+//You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
+//
+//You are given a helper function bool knows(a, b) which tells you whether A knows B. 
+//Implement a function int findCelebrity(n), your function should minimize the number of calls to knows.
+//
+//Note: There will be exactly one celebrity if he/she is in the party. 
+//Return the celebrity's label if there is a celebrity in the party. If there is no celebrity, return -1.
+
+
+//Linkedin
+public class FindCelebrity {
+	/* The knows API is defined in the parent class Relation.
+    boolean knows(int a, int b); */
+	
+	public FindCelebrity() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public void run() {
+		
+	}
+	
+
+	//AC: 25%
+	//Rule:  knows(A, B)
+	//Yes ==> A is NOT celebrity; B might be; need to further confirm with knows(B,A), only when knows(B, A)==false, B will be candidate celebrity
+	//No ==> A might be;  B is NOT celebrity
+	//
+	//Attention: for non-celebrity, they might know each other mutually
+    public int findCelebrity(int n) {
+        List<Integer> lstCandidate = new ArrayList<Integer>();
+        int nCelId = -1;
+        int i;
+        boolean bAnswer = false;
+        int nNonCandidate = -1;
+        
+        for (i=0; i<n/2; i++) {
+        	bAnswer = knows(i, n-1-i);
+        	if (bAnswer == true) {
+        		if (!knows(n-1-i, i)) {        			
+        		    lstCandidate.add(n-1-i);
+        		    nNonCandidate = i;
+        		}
+        	} else {
+        		lstCandidate.add(i);
+        		nNonCandidate = n-1-i;
+        	}
+        }
+        
+        if (n % 2 == 1) {
+            if (nNonCandidate != -1) {
+        	    bAnswer = knows(nNonCandidate, n/2);
+        	    if (bAnswer == true) {
+        	        if (!knows(n/2, nNonCandidate)) lstCandidate.add(n/2);
+        	    }
+            } else {
+                lstCandidate.add(n/2);      
+            }
+        }
+        
+        nCelId = findCelebrityHelper(lstCandidate);
+        if (nCelId != -1) {
+        	for (i = 0; i < n; i++) {
+        		if (i != nCelId && (knows(i, nCelId) == false || knows(nCelId, i) == true)) return -1;
+        	}
+        }
+        
+        return nCelId;
+    }	
+    
+    
+    private int findCelebrityHelper(List<Integer> lstCandidate) {
+    	 int n = lstCandidate.size();
+    	 if (n == 0) return -1;
+    	 if (n == 1) return lstCandidate.get(0);
+    	 List<Integer> lstCandidateNew = new ArrayList<Integer>();
+    	 boolean bAnswer = false;
+    	 int i;
+    	 int nNonCandidate = -1;
+    	 
+         for (i=0; i<n/2; i++) {
+         	bAnswer = knows(lstCandidate.get(i), lstCandidate.get(n-1-i));
+         	if (bAnswer == true) {
+         		if (!knows(lstCandidate.get(n-1-i), lstCandidate.get(i))) {
+         		    lstCandidateNew.add(lstCandidate.get(n-1-i));
+         		    nNonCandidate = lstCandidate.get(i);
+         		}
+         	} else {
+         		lstCandidateNew.add(lstCandidate.get(i));
+         		nNonCandidate = lstCandidate.get(n-1-i);
+         	}
+         }
+         
+         if (n % 2 == 1) {
+            if (nNonCandidate != -1) {
+         	    bAnswer = knows(nNonCandidate, lstCandidate.get(n/2));
+         	    if (bAnswer == true) {
+         	        if (!knows(lstCandidate.get(n/2), nNonCandidate)) lstCandidateNew.add(lstCandidate.get(n/2));         		
+         	    }
+            } else {
+                lstCandidateNew.add(lstCandidate.get(n/2)); 
+            }
+         }
+    	 
+         return findCelebrityHelper(lstCandidateNew);
+    }
+    
+}
---
+++ com/leet/InorderSuccessorBST.java
@@ -0,0 +1,137 @@
+package com.leet;
+
+import java.util.Stack;
+
+//Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+//
+//Note: If the given node has no in-order successor in the tree, return null.
+//		
+		
+//Microsoft		
+public class InorderSuccessorBST {
+
+	public InorderSuccessorBST() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public void run() {
+		TreeNode root = new TreeNode(4);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(6);
+		
+		root.left.left = new TreeNode(1);
+		root.left.right = new TreeNode(3);
+		
+		root.right.left = new TreeNode(5);
+		root.right.right = new TreeNode(7);
+		
+		TreeNode p = root.left.right;
+		
+		TreeNode tn = inorderSuccessor(root, p);
+		
+		if (tn == null) {
+			System.out.println("null");
+		} else {
+		    System.out.println(tn.val);
+		}
+	}
+
+	
+	//AC: 40%
+	//Utilize the property of BST and utilize that P is a valid node (not a value)
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null) return null;
+        if (root.left == null && root.right == null && root != p) return null;
+
+        TreeNode tmp = null;
+        
+        if (p.right != null) {
+            tmp = p.right;
+            while (tmp.left != null) tmp = tmp.left;
+            return tmp;
+        }
+        
+        while (root != p) {
+        	if (root.val > p.val) {  //p in left
+        		tmp = root;
+        		root = root.left;
+        	} else {  //p in right
+        		root = root.right;   //when move to right, the successor will still be its (grand) root's (in case right not exist) successor
+        	}
+        }
+        
+        return tmp;
+    }
+    
+    
+    //OR
+//    The idea is to compare root's value with p's value if root is not null, and consider the following two cases:
+//
+//    	root.val > p.val. In this case, root can be a possible answer, so we store the root node first and call it res. 
+//    	However, we don't know if there is anymore node on root's left that is larger than p.val. 
+//    	So we move root to its left and check again.
+//
+//    	root.val <= p.val. In this case, root cannot be p's inorder successor, 
+//    	neither can root's left child. So we only need to consider root's right child, 
+//    	thus we move root to its right and check again.
+//
+//    	We continuously move root until exhausted. To this point, we only need to return the res in case 1.    
+    
+    public TreeNode inorderSuccessorB(TreeNode root, TreeNode p) {
+        TreeNode tmp = null;
+                
+        while (root != null) {
+        	if (root.val > p.val) {  //p in left
+        		tmp = root;
+        		root = root.left;
+        	} else {  //p in right
+        		root = root.right;   //when move to right, the successor will still be its (grand) root's (in case right not exist) successor
+        	}
+        }
+        
+        return tmp;
+    }
+    
+    
+    
+    
+	//AC: 4%
+    public TreeNode inorderSuccessorA(TreeNode root, TreeNode p) {
+        if (root == null) return null;
+        if (root.left == null && root.right == null && root != p) return null;
+        Stack<TreeNode> stkTree = new Stack<TreeNode>();
+        TreeNode prev = null;
+        TreeNode tnCur = null;
+        TreeNode tmp = null;
+        
+        tmp = root;
+        while (tmp != null) {
+        	stkTree.push(tmp);
+        	tmp = tmp.left;
+        }
+        
+        while (!stkTree.isEmpty()) {
+        	tnCur = stkTree.pop();
+        	
+        	if (prev != null && prev == p) return tnCur;
+        	
+        	prev = tnCur;
+        	
+        	if (tnCur.right != null) {
+        		stkTree.push(tnCur.right);
+        		
+        		tmp = tnCur.right;
+        		
+        		while (tmp.left != null) {
+        			stkTree.push(tmp.left);
+        			tmp = tmp.left;
+        		}
+        	}
+        }
+        
+        return null;
+    }
+    
+ 
+}
