4a39c5b738ee33b1772cc7d0608388cbc8a5112b
mengarena <mengrufeng@gmail.com>
2016-03-31 04:08:52 -0400

    Added files via upload

---
+++ com/leet/EncodeDecodeStrings.java
@@ -0,0 +1,120 @@
+package com.leet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Design an algorithm to encode a list of strings to a string. 
+//The encoded string is then sent over the network and is decoded back to the original list of strings.
+//
+//Machine 1 (sender) has the function:
+//
+//string encode(vector<string> strs) {
+//  // ... your code
+//  return encoded_string;
+//}
+//Machine 2 (receiver) has the function:
+//vector<string> decode(string s) {
+//  //... your code
+//  return strs;
+//}
+//So Machine 1 does:
+//
+//string encoded_string = encode(strs);
+//and Machine 2 does:
+//
+//vector<string> strs2 = decode(encoded_string);
+//strs2 in Machine 2 should be the same as strs in Machine 1.
+//
+//Implement the encode and decode methods.
+//
+//Note:
+//The string may contain any possible characters out of 256 valid ascii characters. 
+//Your algorithm should be generalized enough to work on any possible characters.
+//Do not use class member/global/static variables to store states. 
+//Your encode and decode algorithms should be stateless.
+//Do not rely on any library method such as eval or serialize methods. 
+//You should implement your own encode/decode algorithm.
+
+
+//Google
+public class EncodeDecodeStrings {
+
+	public EncodeDecodeStrings() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public void run() {
+		List<String> lstRs = new ArrayList<String>();
+		lstRs.add("meng");
+		lstRs.add("ru");
+		lstRs.add("feng");
+		lstRs.add("happy");
+		lstRs.add("man!");
+		
+		String sEncode = encode(lstRs);
+		
+		System.out.println(sEncode);
+		
+		List<String> lstRR = decode(sEncode);
+		
+		for (String s:lstRR) System.out.println(s);
+		
+		System.out.println();
+	}
+	
+	
+	//AC:  53%
+	
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        if (strs == null || strs.size() == 0) return null;
+        StringBuilder sb = new StringBuilder();
+        int nLen = 0;
+        
+        for (String s:strs) {
+        	nLen = s.length();
+        	sb.append(Integer.toString(nLen));
+        	sb.append("#");
+        	sb.append(s);
+        }
+        
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> lstRet = new ArrayList<String>();
+        
+        if (s == null) return lstRet;
+        int n = s.length();
+        int i;
+        int nLen = 0;
+        int nStartPos = 0;
+        
+        i=0; 
+        nStartPos = 0;
+        while (i < n) {
+        	if (s.charAt(i) != '#') {
+        		i++;
+        		continue;
+        	}
+        	
+        	nLen = Integer.valueOf(s.substring(nStartPos, i)).intValue();
+        	
+        	lstRet.add(s.substring(i+1, i+1+nLen));
+        	
+        	i = i + 1 + nLen;
+        	nStartPos = i;
+        	
+        }
+
+        return lstRet;
+    }
+
+}
+
+//Your Codec object will be instantiated and called as such:
+//Codec codec = new Codec();
+//codec.decode(codec.encode(strs));
+
---
+++ com/leet/LargestBSTSubtree.java
@@ -0,0 +1,171 @@
+package com.leet;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+//Given a binary tree, find the largest subtree which is a Binary Search Tree (BST), where largest means subtree with largest number of nodes in it.
+//
+//Note:
+//A subtree must include all of its descendants.
+//Here's an example:
+//
+//    10
+//    / \
+//   5  15
+//  / \   \ 
+// 1   8   7
+// 
+//The Largest BST Subtree in this case is the highlighted one. 
+//The return value is the subtree's size, which is 3.
+//		
+//Hint:
+//
+//You can recursively use algorithm similar to 98. 
+//Validate Binary Search Tree at each node of the tree, which will result in O(nlogn) time complexity.
+//
+//Follow up:
+//Can you figure out ways to solve it with O(n) time complexity?
+
+
+//Microsoft
+public class LargestBSTSubtree {
+
+	public LargestBSTSubtree() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void run() {
+		
+	}
+	
+	
+	
+	
+	class Result {
+		int nSize;
+		int nUpper;
+		int nLower;
+		
+		Result(int size, int lower, int upper) {
+			nSize = size;
+			nUpper = upper;
+			nLower = lower;
+		}
+	
+	}
+	
+   public int largestBSTSubtree(TreeNode root) {
+        if (root == null) return 0;
+
+        Result ret = traverse(root);
+        
+        return Math.abs(ret.nSize);
+    } 
+ 
+    //When the subtree rooted at "root" is not a BST,
+    //It uses the max size of its left/right valid BST as the max size at "root"
+    //But, since it uses its left/right subtrees, not the subtree rooted at this node, it use negative to represent
+	private Result traverse(TreeNode root) {
+		if (root == null) return new Result(0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+		
+		Result left = traverse(root.left);
+		Result right = traverse(root.right);
+		
+		//Subtree rooted at "root" is not a valid BST, it uses its left/right subtree's max valid size as its valid BST size
+		if (left.nSize < 0 || right.nSize < 0 || root.val <= left.nUpper || root.val >= right.nLower) {
+			return new Result(Math.max(Math.abs(left.nSize), Math.abs(right.nSize))*(-1), 0, 0);
+		}
+		
+		//Valid BST rooted at "root"
+		return new Result(left.nSize + right.nSize + 1, Math.min(root.val, left.nLower), Math.max(root.val, right.nUpper));
+	}   
+    
+	
+	
+  
+   //Works  //AC: 13%
+   private static int nMax = 1;
+	
+   public int largestBSTSubtreeA(TreeNode root) {
+        if (root == null) return 0;
+
+        traverse(root);
+        
+        return nMax;
+    }
+	   
+	private Result traverseA(TreeNode root) {
+		if (root == null) return new Result(0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+		
+		Result left = traverse(root.left);
+		Result right = traverse(root.right);
+		
+		if (left.nSize == -1 || right.nSize == -1 || root.val <= left.nUpper || root.val >= right.nLower) {
+			return new Result(-1, 0, 0);
+		}
+		
+		int nSize = left.nSize + right.nSize + 1;
+		nMax = Math.max(nMax, nSize);
+		
+		return new Result(nSize, Math.min(root.val, left.nLower), Math.max(root.val, right.nUpper));
+	}
+    
+	
+	
+	
+	
+	//AC:  10%    
+    public int largestBSTSubtreeB(TreeNode root) {
+        if (root == null) return 0;
+        TreeNode tmp = null;
+        int nMax = 1;
+        
+        Queue<TreeNode> quTree = new LinkedList<TreeNode>();
+        
+        quTree.offer(root);
+        
+        //Level Traversal
+        while (!quTree.isEmpty()) {
+        	tmp = quTree.poll();
+        	
+        	List<Integer> lstTree = new ArrayList<Integer>();
+        	
+        	if ((tmp.left != null || tmp.right != null) && isValidBST(tmp, lstTree)) {
+        	    if (tmp == root) return lstTree.size();
+        	    
+        	    nMax = Math.max(nMax, lstTree.size());
+        	}
+        	
+        	if (tmp.left != null) quTree.offer(tmp.left);
+        	if (tmp.right != null) quTree.offer(tmp.right);
+        }
+        
+        return nMax;
+    }    
+    
+    
+    public boolean isValidBST(TreeNode root, List<Integer> lstNode) {
+    	if (root == null) return true;
+    	
+    	if (root.left != null) {
+    		if (isValidBST(root.left, lstNode) == false) return false;
+    	}
+    	
+    	if (lstNode.isEmpty()) {
+    		lstNode.add(root.val);
+    	} else {
+    		if (lstNode.get(lstNode.size()-1) >= root.val) return false;
+    		lstNode.add(root.val);	
+    	}
+    	
+    	if (root.right != null) {
+    		if (isValidBST(root.right, lstNode) == false) return false;
+    	}
+    	
+    	return true;
+    }
+
+}
