55fdd5feb688025556b5561fbb79e7fc2985cea3
mengarena <mengrufeng@gmail.com>
2016-04-13 01:39:18 -0400

    Added files via upload

--- com/leet/BuySellStockWithCooldown.java
+++ com/leet/BuySellStockWithCooldown.java
@@ -24,14 +24,56 @@ public class BuySellStockWithCooldown {
 		// TODO Auto-generated constructor stub
 	}
 
+//	http://bookshadow.com/weblog/2015/11/24/leetcode-best-time-to-buy-and-sell-stock-with-cooldown/#0-tsina-1-7114-397232819ff9a47a7b7e80a40613cfe1
+//	
+//	sells[i]  Max profit on day i without holding stock
+//	buys[i]   Max profit on day i with holding stock
+//
+//	Initialization:£º
+//	sells[0] = 0
+//	sells[1] = max(0, prices[1] - prices[0])
+//	buys[0] = -prices[0]
+//	buys[1] = max(-prices[0], -prices[1])
+//	
+//	State transaction:£º
+//	sells[i] = max(sells[i - 1], buys[i - 1] + prices[i])
+//	buys[i] = max(buys[i - 1], sells[i - 2] - prices[i])
+// 
+//  The maximal profit is sells[-1]	  //i.e. Last one
+	
+	//ACC
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) return 0;
+        int n = prices.length;
+        int[] sells = new int[n];   //Max profit by day i with a sell on day i
+        int[] buys = new int[n];    //Max profit by day i with a buy on day i
+        if (n <= 1) return 0;
+        
+        sells[0] = 0; 
+        sells[1] = Math.max(0, prices[1] - prices[0]);
+        buys[0] = -prices[0];   
+        buys[1] = Math.max(-prices[0], -prices[1]);
+        
+        for (int i=2; i<n; i++) {
+            sells[i] = Math.max(sells[i-1], buys[i-1] + prices[i]);  //do nothing  OR   do a sell based on what has bought by day i-1
+            buys[i] = Math.max(buys[i-1], sells[i-2] - prices[i]);   //do nothing  OR   do a buy based on the profit from sells[i-2] (i.e. there is a cooldown day between)
+        }
 
+        return sells[n-1];
+    }	
+	
+    
+    
 	public void run() {
 		int[] prices = {1, 2, 3, 0, 2};
 		
 		System.out.println("Max Profit: " + maxProfit(prices));
 	}
 	
-	public int maxProfit(int[] prices) {
+	
+	
+	
+	public int maxProfitA(int[] prices) {
 		int n = prices.length;
 		if (n <= 1) return 0;
 		int[] narrDpProfit = new int[n];
@@ -45,6 +87,17 @@ public class BuySellStockWithCooldown {
 		
 		return narrDpProfit[n-1];
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 /*	
     public int maxProfit(int[] prices) {
