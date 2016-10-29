7dba6571891d54f80f33b8a26af989cdf7003642
Meng's Personal Project Code <mengrufeng@gmail.com>
2016-10-28 21:40:11 -0400

    Update LowestCommonAncestorBST.java

--- com/leet/LowestCommonAncestorBST.java
+++ com/leet/LowestCommonAncestorBST.java
@@ -6,8 +6,8 @@ import java.util.List;
 //Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
 //
 //According to the definition of LCA on Wikipedia: 
-//¡°The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants 
-//(where we allow a node to be a descendant of itself).¡±
+//Â¡Â°The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants 
+//(where we allow a node to be a descendant of itself).Â¡Â±
 //
 //      _______6______
 //     /               \
@@ -19,7 +19,8 @@ import java.util.List;
 //For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another example is LCA of nodes 2 and 4 is 2, 
 //since a node can be a descendant of itself according to the LCA definition.
 
-//Facebook, Amazon
+//Facebook, Amazon, Microsoft, Twitter
+//Easy
 public class LowestCommonAncestorBST {
 	 //Definition for a binary tree node.
 	 public class TreeNode {
