7c328d81f03857c07016c793c5d7da22dbe8d118
Meng's Personal Project Code <mengrufeng@gmail.com>
2016-10-28 22:02:09 -0400

    Update MergeSortedArray.java

--- com/leet/MergeSortedArray.java
+++ com/leet/MergeSortedArray.java
@@ -6,7 +6,8 @@ package com.leet;
 //You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. 
 //The number of elements initialized in nums1 and nums2 are m and n respectively.
 
-//Facebook, Bloomberg
+//Facebook, Bloomberg, Microsoft
+//Easy
 public class MergeSortedArray {
 
 	public MergeSortedArray() {
