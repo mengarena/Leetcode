59b118025bfcf184e82490afbde59fc5ccc3b8cd
Meng's Personal Project Code <mengrufeng@gmail.com>
2016-10-28 21:09:49 -0400

    Update NestedListWeightSum.java

--- com/leet/NestedListWeightSum.java
+++ com/leet/NestedListWeightSum.java
@@ -13,6 +13,7 @@ import java.util.List;
 //Given the list [1,[4,[6]]], return 27. (one 1 at depth 1, one 4 at depth 2, and one 6 at depth 3; 1 + 4*2 + 6*3 = 27)
 
 //Linkedin
+//Easy
 public class NestedListWeightSum {
 
 	public NestedListWeightSum() {
