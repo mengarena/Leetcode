5fc6909ba22bbd229dfadf86eb865af57e112a81
Meng's Personal Project Code <mengrufeng@gmail.com>
2016-10-28 22:37:06 -0400

    Update RussianDollEnvelopes.java

--- com/leet/RussianDollEnvelopes.java
+++ com/leet/RussianDollEnvelopes.java
@@ -12,7 +12,8 @@ import java.util.Comparator;
 //	
 //Given envelopes = [[5,4],[6,4],[6,7],[2,3]], the maximum number of envelopes you can Russian doll is 3 ([2,3] => [5,4] => [6,7]).
 
-
+//Google
+//Hard
 public class RussianDollEnvelopes {
 
 	public RussianDollEnvelopes() {
