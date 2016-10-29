44e6bf5b685ca2e9ccf02e13ee78b363bca64f2a
Meng's Personal Project Code <mengrufeng@gmail.com>
2016-10-28 21:50:48 -0400

    Update RemoveDuplicatesSortedArray.java

--- com/leet/RemoveDuplicatesSortedArray.java
+++ com/leet/RemoveDuplicatesSortedArray.java
@@ -10,7 +10,8 @@ package com.leet;
 //Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. 
 //It doesn't matter what you leave beyond the new length.
 
-//Facebook, Bloomberg
+//Facebook, Bloomberg, Microsoft
+//Easy
 public class RemoveDuplicatesSortedArray {
 
 	public RemoveDuplicatesSortedArray() {
