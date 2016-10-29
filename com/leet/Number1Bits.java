1073bf44b05c86d1171f379d3b939a4a25216fe3
Meng's Personal Project Code <mengrufeng@gmail.com>
2016-10-28 21:38:43 -0400

    Update Number1Bits.java

--- com/leet/Number1Bits.java
+++ com/leet/Number1Bits.java
@@ -1,9 +1,11 @@
 package com.leet;
 
-//Write a function that takes an unsigned integer and returns the number of ¡¯1' bits it has (also known as the Hamming weight).
+//Write a function that takes an unsigned integer and returns the number of Â¡Â¯1' bits it has (also known as the Hamming weight).
 //
-//For example, the 32-bit integer ¡¯11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+//For example, the 32-bit integer Â¡Â¯11' has binary representation 00000000000000000000000000001011, so the function should return 3.
 
+//Apple, Microsoft
+//Easy
 public class Number1Bits {
 
 	public Number1Bits() {
