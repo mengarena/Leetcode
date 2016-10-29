51b4fa2cd60d77c2ef891b63a186708380b076a4
Meng's Personal Project Code <mengrufeng@gmail.com>
2016-10-28 21:22:53 -0400

    Update DeleteNodeLinkedList.java

--- com/leet/DeleteNodeLinkedList.java
+++ com/leet/DeleteNodeLinkedList.java
@@ -5,6 +5,8 @@ package com.leet;
 //Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, 
 //the linked list should become 1 -> 2 -> 4 after calling your function.
 
+//Apple,Adobe,Microsoft
+//Easy
 public class DeleteNodeLinkedList {
 
 	//Definition for singly-linked list.
