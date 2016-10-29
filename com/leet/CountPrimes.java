ca7a2e8a6077afbfe5ff8230ee37e726fcc51978
Meng's Personal Project Code <mengrufeng@gmail.com>
2016-10-28 22:12:34 -0400

    Update CountPrimes.java

--- com/leet/CountPrimes.java
+++ com/leet/CountPrimes.java
@@ -19,12 +19,12 @@ import java.util.List;
 //
 //Let's write down all of 12's factors:
 //
-//2 กม 6 = 12
-//3 กม 4 = 12
-//4 กม 3 = 12
-//6 กม 2 = 12
-//As you can see, calculations of 4 กม 3 and 6 กม 2 are not necessary. 
-//Therefore, we only need to consider factors up to กฬn because, if n is divisible by some number p, then n = p กม q and since p กÜ q, we could derive that p กÜ กฬn.
+//2 ยกร 6 = 12
+//3 ยกร 4 = 12
+//4 ยกร 3 = 12
+//6 ยกร 2 = 12
+//As you can see, calculations of 4 ยกร 3 and 6 ยกร 2 are not necessary. 
+//Therefore, we only need to consider factors up to ยกรn because, if n is divisible by some number p, then n = p ยกร q and since p ยกร q, we could derive that p ยกร ยกรn.
 //
 //Our total runtime has now improved to O(n1.5), which is slightly better. Is there a faster approach?
 //
@@ -53,23 +53,23 @@ import java.util.List;
 //
 //We start off with a table of n numbers. Let's look at the first number, 2. 
 //We know all multiples of 2 must not be primes, so we mark them off as non-primes. 
-//Then we look at the next number, 3. Similarly, all multiples of 3 such as 3 กม 2 = 6, 3 กม 3 = 9, ... must not be primes, 
+//Then we look at the next number, 3. Similarly, all multiples of 3 such as 3 ยกร 2 = 6, 3 ยกร 3 = 9, ... must not be primes, 
 //so we mark them off as well. Now we look at the next number, 4, which was already marked off. 
 //What does this tell you? Should you mark off all multiples of 4 as well?
 //
 //4 is not a prime because it is divisible by 2, which means all multiples of 4 must also be divisible by 2 and were already marked off. 
 //So we can skip 4 immediately and go to the next number, 5. 
-//Now, all multiples of 5 such as 5 กม 2 = 10, 5 กม 3 = 15, 5 กม 4 = 20, 5 กม 5 = 25, ... can be marked off. 
-//There is a slight optimization here, we do not need to start from 5 กม 2 = 10. Where should we start marking off?
+//Now, all multiples of 5 such as 5 ยกร 2 = 10, 5 ยกร 3 = 15, 5 ยกร 4 = 20, 5 ยกร 5 = 25, ... can be marked off. 
+//There is a slight optimization here, we do not need to start from 5 ยกร 2 = 10. Where should we start marking off?
 //
-//In fact, we can mark off multiples of 5 starting at 5 กม 5 = 25, 
-//because 5 กม 2 = 10 was already marked off by multiple of 2, similarly 5 กม 3 = 15 was already marked off by multiple of 3. 
+//In fact, we can mark off multiples of 5 starting at 5 ยกร 5 = 25, 
+//because 5 ยกร 2 = 10 was already marked off by multiple of 2, similarly 5 ยกร 3 = 15 was already marked off by multiple of 3. 
 //Therefore, if the current number is p, we can always mark off multiples of p starting at p2, then in increments of p: p2 + p, p2 + 2p, ... 
 //Now what should be the terminating loop condition?
 //
 //It is easy to say that the terminating loop condition is p < n, which is certainly correct but not efficient. Do you still remember Hint #3?
 //
-//Yes, the terminating loop condition can be p < กฬn, as all non-primes กÝ กฬn must have already been marked off. 
+//Yes, the terminating loop condition can be p < ยกรn, as all non-primes ยกร ยกรn must have already been marked off. 
 //When the loop terminates, all the numbers in the table that are non-marked are prime.
 //
 //The Sieve of Eratosthenes uses an extra O(n) memory and its runtime complexity is O(n log log n). 
@@ -95,7 +95,8 @@ import java.util.List;
 //   return count;
 //}
 
-//Amazon
+//Amazon, Microsoft
+//Easy
 public class CountPrimes {
 
 	public CountPrimes() {
