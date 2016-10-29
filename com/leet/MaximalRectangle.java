19f614b590c0c1741ec1936f6b5bc9eba60c269d
Meng's Personal Project Code <mengrufeng@gmail.com>
2016-10-28 22:58:37 -0400

    Update MaximalRectangle.java

--- com/leet/MaximalRectangle.java
+++ com/leet/MaximalRectangle.java
@@ -3,7 +3,18 @@ package com.leet;
 import java.util.Stack;
 
 //Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing all(only) ones and return its area.
+//
+//For example, given the following matrix:
 
+//1 0 1 0 0
+//1 0 1 1 1
+//1 1 1 1 1
+//1 0 0 1 0
+	
+//Return 6.
+
+//Facebook
+//Hard
 public class MaximalRectangle {
 
 	public MaximalRectangle() {
