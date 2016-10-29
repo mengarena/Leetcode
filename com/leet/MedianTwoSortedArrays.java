1df36a70adba562f204fe3d400373345aeccf76c
Meng's Personal Project Code <mengrufeng@gmail.com>
2016-10-28 23:20:44 -0400

    Update MedianTwoSortedArrays.java

--- com/leet/MedianTwoSortedArrays.java
+++ com/leet/MedianTwoSortedArrays.java
@@ -6,7 +6,8 @@ import java.util.PriorityQueue;
 //Find the median of the two sorted arrays. 
 //The overall run time complexity should be O(log (m+n)).
 
-//Google, Uber, Zenefits
+//Google, Uber, Zenefits, Microsoft, Apple, Yahoo, Adobe, Dropbox
+//Hard
 public class MedianTwoSortedArrays {
 
 	public MedianTwoSortedArrays() {
@@ -20,7 +21,65 @@ public class MedianTwoSortedArrays {
     	System.out.println(findMedianSortedArrays(nums1, nums2));
     }
     
+	
+//The key point of this problem is to ignore half part of A and B each step recursively by comparing the median of remaining A and B:
+
+//if (aMid < bMid) Keep [aRight + bLeft]
+//else Keep [bRight + aLeft]
+
+//As the following: time=O(log(m + n))
+        //ACC
+        public double findMedianSortedArrays(int[] A, int[] B) {
+	    int m = A.length, n = B.length;
+	    int l = (m + n + 1) / 2;
+	    int r = (m + n + 2) / 2;
+	    return (getkth(A, 0, B, 0, l) + getkth(A, 0, B, 0, r)) / 2.0;
+	}
+
+        public double getkth(int[] A, int aStart, int[] B, int bStart, int k) {
+	    if (aStart > A.length - 1) return B[bStart + k - 1];            
+	    if (bStart > B.length - 1) return A[aStart + k - 1];                
+	    if (k == 1) return Math.min(A[aStart], B[bStart]);
+	
+	    int aMid = Integer.MAX_VALUE, bMid = Integer.MAX_VALUE;
+	    if (aStart + k/2 - 1 < A.length) aMid = A[aStart + k/2 - 1]; 
+	    if (bStart + k/2 - 1 < B.length) bMid = B[bStart + k/2 - 1];        
+	
+	    if (aMid < bMid) 
+	        return getkth(A, aStart + k/2, B, bStart,       k - k/2);// Check: aRight + bLeft 
+	    else 
+		return getkth(A, aStart,       B, bStart + k/2, k - k/2);// Check: bRight + aLeft
+       }	
+	
+	
+//Binary search. Call 2 times getkth and k is about half of (m + n). 
+//Every time call getkth can reduce the scale k to its half. So the time complexity is log(m + n).
+    //ACC	
+    public int getkth(int s[], int m, int l[], int n, int k){
+        // let m <= n
+        if (m > n) 
+            return getkth(l, n, s, m, k);
+        if (m == 0)
+            return l[k - 1];
+        if (k == 1)
+            return min(s[0], l[0]);
+
+        int i = min(m, k / 2), j = min(n, k / 2);
+        if (s[i - 1] > l[j - 1])
+            return getkth(s, m, l + j, n - j, k - j);
+        else
+            return getkth(s + i, m - i, l, n, k - i);
+        return 0;
+    }
+    
+    public double findMedianSortedArrays(int A[], int m, int B[], int n) {
+        int l = (m + n + 1) >> 1;
+        int r = (m + n + 2) >> 1;
+        return (getkth(A, m ,B, n, l) + getkth(A, m, B, n, r)) / 2.0;
+    }	
+	
  
+	
     //ACC:  47%
     public double findMedianSortedArraysM(int[] nums1, int[] nums2) {
         if ((nums1 == null || nums1.length == 0) && (nums2 == null || nums2.length == 0)) return 0;
