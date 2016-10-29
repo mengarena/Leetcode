4a42f9f5dd72cc60a9b485bcc1d5343527c1552f
Meng's Personal Project Code <mengrufeng@gmail.com>
2016-10-28 23:01:23 -0400

    Update ReadNCharactersGivenRead4IICallMultipleTimes.java

--- com/leet/ReadNCharactersGivenRead4IICallMultipleTimes.java
+++ com/leet/ReadNCharactersGivenRead4IICallMultipleTimes.java
@@ -14,6 +14,7 @@ import java.util.Queue;
 //The read function may be called multiple times.
 
 //Google, Facebook, Bloomberg
+//Hard
 public class ReadNCharactersGivenRead4IICallMultipleTimes {
 
 	public ReadNCharactersGivenRead4IICallMultipleTimes() {
