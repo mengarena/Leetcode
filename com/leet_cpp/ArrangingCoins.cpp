01d7232e248ac866637c7f4e41ec1fe441044d84
Meng's Personal Project Code <mengrufeng@gmail.com>
2016-10-31 05:21:59 -0400

    Add files via upload

---
+++ com/leet_cpp/ArrangingCoins.cpp
@@ -0,0 +1,57 @@
+/*
+Arranging Coins
+
+You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+
+Given n, find the total number of full staircase rows that can be formed.
+
+n is a non-negative integer and fits within the range of a 32-bit signed integer.
+
+Example 1:
+
+n = 5
+
+The coins can form the following rows:
+ก่
+ก่ ก่
+ก่ ก่
+
+Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+n = 8
+
+The coins can form the following rows:
+ก่
+ก่ ก่
+ก่ ก่ ก่
+ก่ ก่
+
+Because the 4th row is incomplete, we return 3.
+*/
+
+//GoDaddy
+//Easy
+
+//ACC
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int i = 1;
+        int sum = 1;
+        
+        if (n <= 0) return 0;
+        
+        while (sum <= n) {
+            i++;
+            if (sum <= n-i) {
+                sum += i;
+            } else {
+                break;
+            }
+        }
+        
+        return i-1;
+    }
+};
