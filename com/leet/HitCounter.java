3835cc85475311c359dd299a46ac3f15a23dd6e0
Meng's Personal Project Code <mengrufeng@gmail.com>
2016-11-02 21:16:53 -0400

    Ok

--- com/leet/HitCounter.java
+++ com/leet/HitCounter.java
@@ -38,6 +38,8 @@ import java.util.PriorityQueue;
 //Follow up:
 //What if the number of hits per second could be very large? Does your design scale?
 
+//--- means: many hits has same timestamp, need to remember the count of hit for each timestamp, the solution below does not scale
+
 
 //Google, Dropbox		
 public class HitCounter {
@@ -65,6 +67,47 @@ public class HitCounter {
         return pq.size();
     }
 }
+
+
+//ACC:  Scalable
+public class HitCounter {
+    private int[] times;
+    private int[] hits;
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        times = new int[300];
+        hits = new int[300];
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        int index = timestamp % 300;
+        if (times[index] != timestamp) {
+            times[index] = timestamp;
+            hits[index] = 1;
+        } else {
+            hits[index]++;
+        }
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        int total = 0;
+        for (int i = 0; i < 300; i++) {
+            if (timestamp - times[i] < 300) {
+                total += hits[i];
+            }
+        }
+        return total;
+    }
+}
+
+
+
+
+
 
 /**
  * Your HitCounter object will be instantiated and called as such:
