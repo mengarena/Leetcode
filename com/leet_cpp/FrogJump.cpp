354e1cc42d9d0aea5fdc2f0599cec0baa801d7db
mengarena <mengrufeng@gmail.com>
2016-11-20 02:36:19 -0500

    Update FrogJump.cpp

--- com/leet_cpp/FrogJump.cpp
+++ com/leet_cpp/FrogJump.cpp
@@ -8,7 +8,7 @@
 //
 //Note:
 //
-//The number of stones is ¡Ý 2 and is < 1,100.
+//The number of stones is Â¡Ã 2 and is < 1,100.
 //Each stone's position will be a non-negative integer < 231.
 //The first stone's position is always 0.
 //Example 1:
@@ -76,3 +76,4 @@ public:
         return false;
     }
 };
+
