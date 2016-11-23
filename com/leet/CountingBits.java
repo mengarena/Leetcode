2a711775b3abf7e0e98bb1a67839bcfc3c132661
mengarena <mengrufeng@gmail.com>
2016-11-22 20:40:03 -0500

    Update CountingBits.java

--- com/leet/CountingBits.java
+++ com/leet/CountingBits.java
@@ -1,7 +1,7 @@
 package com.leet;
 
 //Given a non negative integer number num. 
-//For every numbers i in the range 0 ¡Ü i ¡Ü num calculate the number of 1's in their binary representation and return them as an array.
+//For every numbers i in the range 0 <= i <= num calculate the number of 1's in their binary representation and return them as an array.
 //
 //Example:
 //For num = 5 you should return [0,1,1,2,1,2].
