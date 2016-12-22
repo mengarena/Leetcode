2086c9e2ce966bbfcc48b4e71eafbba9dab796b8
mengarena <mengrufeng@gmail.com>
2016-12-22 05:35:41 -0500

    added 12 files

--- com/leet/MinimumSizeSubarraySum.java
+++ com/leet/MinimumSizeSubarraySum.java
@@ -2,7 +2,7 @@ package com.leet;
 
 import java.util.Arrays;
 
-//Given an array of n positive integers and a positive integer s, find the minimal length of a subarray of which the sum ¡İ s. 
+//Given an array of n positive integers and a positive integer s, find the minimal length of a subarray of which the sum > s. 
 //If there isn't one, return 0 instead.
 //
 //For example, given the array [2,3,1,2,4,3] and s = 7,
