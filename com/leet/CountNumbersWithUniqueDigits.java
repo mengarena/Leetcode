515e31b95ddd97855d1dd6587de25358e1e1f9c4
mengarena <mengrufeng@gmail.com>
2017-02-13 03:18:37 -0500

    Update CountNumbersWithUniqueDigits.java

--- com/leet/CountNumbersWithUniqueDigits.java
+++ com/leet/CountNumbersWithUniqueDigits.java
@@ -1,10 +1,10 @@
 package com.leet;
 
 
-//Given a non-negative integer n, count all numbers with unique digits, x, where 0 ¡Ü x < 10n.
+//Given a non-negative integer n, count all numbers with unique digits, x, where 0 <= x < 10^n.
 //
 //Example:
-//Given n = 2, return 91. (The answer should be the total numbers in the range of 0 ¡Ü x < 100, excluding [11,22,33,44,55,66,77,88,99])
+//Given n = 2, return 91. (The answer should be the total numbers in the range of 0 <= x < 100, excluding [11,22,33,44,55,66,77,88,99])
 //
 //Hint:
 //
