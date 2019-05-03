b87952602d8d2f948694ecf2816e080c4297d660
mengarena <mengrufeng@gmail.com>
2019-05-03 10:31:38 -0700

    Update LongestZigZagSubsequence.java

--- com/leet/LongestZigZagSubsequence.java
+++ com/leet/LongestZigZagSubsequence.java
@@ -2,11 +2,12 @@ package com.leet;
 
 //Also called longest alternating subsequence
 
-//The longest Zig-Zag subsequence problem is to find length of the longest subsequence of given sequence such that all elements of this are alternating.
+//The longest Zig-Zag subsequence problem is to find length of the longest subsequence of given sequence such that 
+//all elements of this are alternating.
 //If a sequence {x1, x2, .. xn} is alternating sequence then its element satisfy one of the following relation :
 //
-//  x1 < x2 > x3 < x4 > x5 < ¡­. xn or 
-//  x1 > x2 < x3 > x4 < x5 > ¡­. xn 
+//  x1 < x2 > x3 < x4 > x5 < Â¡Â­. xn or 
+//  x1 > x2 < x3 > x4 < x5 > Â¡Â­. xn 
 //Examples:
 //
 //Input: arr[] = {1, 5, 4}
@@ -25,11 +26,13 @@ package com.leet;
 //are longest Zig-Zag of length 6.
 //We strongly recommend you to minimize your browser and try this yourself first.
 //
-//This problem is an extension of longest increasing subsequence problem, but requires more thinking for finding optimal substructure property in this.
+//This problem is an extension of longest increasing subsequence problem, 
+//but requires more thinking for finding optimal substructure property in this.
 //
 //We will solve this problem by dynamic Programming method, Let A is given array of length n of integers. 
 //We define a 2D array Z[n][2] such that Z[i][0] contains longest Zig-Zag subsequence ending at index i and 
-//last element is greater than its previous element and Z[i][1] contains longest Zig-Zag subsequence ending at index i and last element is smaller than its previous element, 
+//last element is greater than its previous element and Z[i][1] contains longest Zig-Zag subsequence ending at index i 
+//and last element is smaller than its previous element, 
 //then we have following recurrence relation between them,
 //
 //Z[i][0] = Length of the longest Zig-Zag subsequence 
@@ -48,10 +51,12 @@ package com.leet;
 //The first recurrence relation is based on the fact that, 
 //If we are at position i and this element has to be bigger than its previous element then 
 //for this sequence (upto i) to be bigger we will try to choose an element j ( < i) such that
-//A[j] < A[i] i.e. A[j] can become A[i]¡¯s previous element and Z[j][1] + 1 is bigger than Z[i][0] then we will update Z[i][0].
+//A[j] < A[i] i.e. A[j] can become A[i]Â¡Â¯s previous element and Z[j][1] + 1 is bigger than Z[i][0] then 
+//we will update Z[i][0].
 //
 //Remember we have chosen Z[j][1] + 1 not Z[j][0] + 1 to satisfy alternate property 
-//because in Z[j][0] last element is bigger than its previous one and A[i] is greater than A[j] which will break the alternating property if we update. 
+//because in Z[j][0] last element is bigger than its previous one and A[i] is greater than A[j] 
+//which will break the alternating property if we update. 
 //So above fact derives first recurrence relation, similar argument can be made for second recurrence relation also.
 //
 
