210f8e8096adf09e1bc8fe59d41cfe164f434467
mengarena <mengrufeng@gmail.com>
2019-05-08 11:09:39 -0700

    Update NumberOfDigitOne.java

--- com/leet/NumberOfDigitOne.java
+++ com/leet/NumberOfDigitOne.java
@@ -39,7 +39,8 @@ public class NumberOfDigitOne {
 	public int countDigitOne(int n) {
 		int ones = 0;
 		for (long m = 1; m <= n; m *= 10)
-			ones += (n/m + 8) / 10 * m + (n/m % 10 == 1 ? n%m + 1 : 0);
+		    ones += (n/m + 8) / 10 * m + (n/m % 10 == 1 ? n%m + 1 : 0);
+		
 		return ones;
 	}
 
