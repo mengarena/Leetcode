eb92c7724496e8b45df9c64bcb222da06b7fe13b
mengarena <mengrufeng@gmail.com>
2019-05-16 22:14:34 -0700

    Update FindKClosestElements.cpp

--- com/leet_cpp/FindKClosestElements.cpp
+++ com/leet_cpp/FindKClosestElements.cpp
@@ -29,6 +29,91 @@ Uber
 
 class Solution {
 public:
+    
+    // 70%
+    // Time complixity: O(logn) --- n is the number of elements in arr (actually should be log(n-k))
+    // This method direct searchs for the left and right boundary of the k-element section within arr,
+    // which contains the K closest elements
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left = 0, right = arr.size()-k;
+        
+        // The 'mid' is the beginning of the candidate section (i.e. the section is mid ~ mid+k
+        // i.e. arr[mid] and arr[mid+k] and two boundary elements
+        // push the K-element section window until arr[mid] is closer to x (at that time, left = right)
+        while (left < right) {
+            int mid = left + (right-left)/2;
+            if (x-arr[mid] > arr[mid+k] - x) left = mid+1;
+            else right = mid;
+        }
+
+        return vector<int>(arr.begin()+left, arr.begin()+left+k);
+    }
+
+    // 5%
+    // Prune from begin and end
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int> ans = arr;
+ 
+        while (ans.size() > k) {
+            if (abs(ans.front() - x) > abs(ans.back() -x)) {
+                ans.erase(ans.begin());
+            } else {
+                ans.pop_back();
+            }
+        }
+        
+        return ans;
+    }
+    
+    
+    // 10%
+    // Strategy:  Find the insert position in arr for x
+    // From the insert position, check both directions
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int> ans;
+        int i = 0;
+        int j = arr.size()-1;
+        int insertPos = j;
+        
+        // Search for the insert position for x in arr
+        while (i < j) {
+            int mid = i + (j-i)/2;
+            if (arr[mid] > x) {
+                insertPos = mid;
+                if (arr[mid] == x) break;
+                j = mid;
+            } else {
+                i = mid + 1;
+            }
+        }
+        
+        j = insertPos;
+        i = j-1;
+        
+        while (ans.size() < k && i >= 0 && j < arr.size()) {            
+            if (abs(arr[i] - x) <= abs(arr[j] - x)) {
+                ans.insert(ans.begin(), arr[i--]);
+            } else {
+                ans.push_back(arr[j++]);
+            }
+        }
+        
+        while (ans.size() < k) {
+            if (i >= 0) {
+                ans.insert(ans.begin(), arr[i--]);
+            }
+            
+            if (j < arr.size()) {
+                ans.push_back(arr[j++]);
+            }
+        }
+        
+        return ans;
+    }    
+    
+    
+    
+    
     // 12%
     // Strategy: First check whether x is beyond the range of arr, if that's the case, it becomes easy,
     // just get the elements continusously from the begin or end
