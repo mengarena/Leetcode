0b36821013ed3e736ed9164629abaf884caa69f0
mengarena <mengrufeng@gmail.com>
2019-05-20 12:10:28 -0700

    Create BinaryTreeTilt.cpp

---
+++ com/leet_cpp/BinaryTreeTilt.cpp
@@ -0,0 +1,71 @@
+/*
+
+563. Binary Tree Tilt
+
+Given a binary tree, return the tilt of the whole tree.
+
+The tilt of a tree node is defined as the absolute difference between the sum of all left subtree node values and
+the sum of all right subtree node values. Null node has tilt 0.
+
+The tilt of the whole tree is defined as the sum of all nodes' tilt.
+
+Example:
+Input: 
+         1
+       /   \
+      2     3
+Output: 1
+
+Explanation: 
+Tilt of node 2 : 0
+Tilt of node 3 : 0
+Tilt of node 1 : |2-3| = 1
+Tilt of binary tree : 0 + 0 + 1 = 1
+
+
+Note:
+
+The sum of node values in any subtree won't exceed the range of 32-bit integer.
+All the tilt values won't exceed the range of 32-bit integer.
+
+Easy
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    // 94%
+    int findTilt(TreeNode* root) {
+        if (!root) return 0;
+        int tilt = 0;
+        
+        getSum(root, tilt);
+
+        return tilt;
+    }
+    
+    int getSum(TreeNode* root, int& tilt) {
+        if (!root) {
+            tilt = 0;
+            return 0;
+        }
+        
+        int tiltLeft = 0;
+        int tiltRight = 0;
+        int sumLeft = getSum(root->left, tiltLeft);
+        int sumRight = getSum(root->right, tiltRight);
+        
+        tilt = abs(sumLeft - sumRight) + tiltLeft + tiltRight;
+        return root->val + sumLeft + sumRight;
+    }
+
+};
