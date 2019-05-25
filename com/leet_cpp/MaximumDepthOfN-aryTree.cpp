b7edbf4eafd56e4ffc27d5336fbe8c863e41e759
mengarena <mengrufeng@gmail.com>
2019-05-25 10:59:16 -0700

    Create MaximumDepthOfN-aryTree.cpp

---
+++ com/leet_cpp/MaximumDepthOfN-aryTree.cpp
@@ -0,0 +1,55 @@
+/*
+
+559. Maximum Depth of N-ary Tree
+
+Given a n-ary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+For example, given a 3-ary tree:
+
+ 
+We should return its max depth, which is 3.
+
+Note:
+
+The depth of the tree is at most 1000.
+The total number of nodes is at most 5000.
+
+Easy
+*/
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+
+    // 92%
+    // Complexity: O(n)
+    int maxDepth(Node* root) {
+        if (!root) return 0;
+        vector<Node*> children = root->children;
+        int maxDep = 0;
+        
+        for (auto child:children) {
+            maxDep = max(maxDep, maxDepth(child));
+        }
+        
+        return maxDep + 1;
+    }
+};
