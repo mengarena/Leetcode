d050666d4fa97f6c27242373a9d11fb0983f2511
mengarena <mengrufeng@gmail.com>
2019-06-15 21:28:13 -0700

    Create DuplicateZeros.cpp

---
+++ com/leet_cpp/DuplicateZeros.cpp
@@ -0,0 +1,76 @@
+/*
+
+1089. Duplicate Zeros
+
+Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+Note that elements beyond the length of the original array are not written.
+
+Do the above modifications to the input array in place, do not return anything from your function.
+
+ 
+Example 1:
+Input: [1,0,2,3,0,4,5,0]
+Output: null
+Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+
+Example 2:
+Input: [1,2,3]
+Output: null
+Explanation: After calling your function, the input array is modified to: [1,2,3]
+ 
+
+Note:
+1 <= arr.length <= 10000
+0 <= arr[i] <= 9
+
+Easy
+*/
+
+
+
+class Solution {
+public:
+
+    // Better
+    void duplicateZeros(vector<int>& arr) {
+        if (arr.size() == 0) return;
+        int n = arr.size();
+        int j = n + count(arr.begin(), arr.end(), 0);
+        
+        for (int i = n-1; i>=0; --i) {
+            if (--j < n) {
+                arr[j] = arr[i];
+            }
+            
+            if (arr[i] == 0 && --j < n) arr[j] = 0;
+        }
+    }
+    
+
+    void duplicateZeros(vector<int>& arr) {
+        if (arr.size() == 0) return;
+        int n = arr.size();
+        int cnt = 0;
+        for (auto num:arr) {
+            if (num == 0) cnt++;
+        }
+
+        if (cnt == 0 || cnt == n || (cnt == 1 && arr[n-1] == 0)) return;
+        
+        for (int i=n-1; i>=0; --i) {
+            if (cnt > 0) {
+                if (i+cnt > n-1) {
+                    if (arr[i] == 0) cnt--;
+                    arr[i] = 0;
+                    continue;
+                }
+                arr[i+cnt] = arr[i];
+                if (arr[i] == 0) {
+                    arr[i+cnt-1] = 0;
+                    cnt--;
+                }  
+            } 
+        }
+    }
+};
