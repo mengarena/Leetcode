3dbc77ccb8cec5dffdc9b0c88653b282a63e5636
mengarena <mengrufeng@gmail.com>
2019-06-18 11:50:08 -0700

    Update WiggleSortII.cpp

--- com/leet_cpp/WiggleSortII.cpp
+++ com/leet_cpp/WiggleSortII.cpp
@@ -24,6 +24,20 @@ Medium
 
 class Solution {
 public:
+
+/*
+Accessing A(0) actually accesses nums[1].
+Accessing A(1) actually accesses nums[3].
+Accessing A(2) actually accesses nums[5].
+Accessing A(3) actually accesses nums[7].
+Accessing A(4) actually accesses nums[9].
+Accessing A(5) actually accesses nums[0].
+Accessing A(6) actually accesses nums[2].
+Accessing A(7) actually accesses nums[4].
+Accessing A(8) actually accesses nums[6].
+Accessing A(9) actually accesses nums[8]. 
+*/
+    
     
     // 99%
     void wiggleSort(vector<int>& nums) {
