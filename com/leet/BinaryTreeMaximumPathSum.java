96e39500b737a349c0dc8d17547fba2896d41fad
mengarena <mengrufeng@gmail.com>
2019-07-31 16:16:14 -0700

    Update BinaryTreeMaximumPathSum.java

--- com/leet/BinaryTreeMaximumPathSum.java
+++ com/leet/BinaryTreeMaximumPathSum.java
@@ -4,7 +4,8 @@ import java.util.Stack;
 
 //Given a binary tree, find the maximum path sum.
 //
-//For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. 
+//For this problem, a path is defined as any sequence of nodes from some starting node to 
+//any node in the tree along the parent-child connections. 
 //The path does not need to go through the root.
 //
 //For example:
