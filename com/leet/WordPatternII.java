221587c833dcc6aca75cd9753b9a550858c15868
mengarena <mengrufeng@gmail.com>
2019-08-01 00:14:35 -0700

    Update WordPatternII.java

--- com/leet/WordPatternII.java
+++ com/leet/WordPatternII.java
@@ -43,24 +43,25 @@ public class WordPatternII {
     	if (pattern.isEmpty()) return str.isEmpty();
     
     	if (hmPatStr.containsKey(pattern.charAt(0))) {
-    		String sval = hmPatStr.get(pattern.charAt(0));
-    		if (str.length() < sval.length() || !str.substring(0, sval.length()).equals(sval)) return false;
-    		if (wordPatternMatch(pattern.substring(1), str.substring(sval.length()))) return true;
+    	    String sval = hmPatStr.get(pattern.charAt(0));
+    	    if (str.length() < sval.length() || !str.substring(0, sval.length()).equals(sval)) return false;
+    	    if (wordPatternMatch(pattern.substring(1), str.substring(sval.length()))) return true;
     	} else {
-    		int n = str.length();
-    		int nSize = pattern.length();
+    	    int n = str.length();
+    	    int nSize = pattern.length();
     		
-    		for (int i=1; i<=n-nSize+1; i++) {
-    			//Reason for using this "if" statement: If hmPatStr contains this substring, but hmPatStr does not contains the pattern.charAt(0), 
-    			//then a substring will have two different keys, that's not valid in this question. 
-    			if (hmPatStr.containsValue(str.substring(0, i))) continue;
+    	    for (int i=1; i<=n-nSize+1; i++) {
+    	        //Reason for using this "if" statement: If hmPatStr contains this substring, 
+		//but hmPatStr does not contains the pattern.charAt(0), 
+    		//then a substring will have two different keys, that's not valid in this question. 
+    		if (hmPatStr.containsValue(str.substring(0, i))) continue;
     			
-    			hmPatStr.put(pattern.charAt(0), str.substring(0, i));
+    		hmPatStr.put(pattern.charAt(0), str.substring(0, i));
     			
-    			if (wordPatternMatch(pattern.substring(1), str.substring(i))) return true;
+    		if (wordPatternMatch(pattern.substring(1), str.substring(i))) return true;
     			
-    			hmPatStr.remove(pattern.charAt(0));
-    		}
+    		hmPatStr.remove(pattern.charAt(0));
+    	    }
     	}
     	
     	return false;
