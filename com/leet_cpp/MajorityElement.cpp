ee7301a674026560ff0fc1ad837f0dcdad763517
mengarena <mengrufeng@gmail.com>
2019-08-05 16:51:32 -0700

    Update MajorityElement.cpp

--- com/leet_cpp/MajorityElement.cpp
+++ com/leet_cpp/MajorityElement.cpp
@@ -1,3 +1,4 @@
+/*
 169. Majority Element
 
 Given an array of size n, find the majority element. The majority element is the element that appears more than ? n/2 ? times.
@@ -5,7 +6,9 @@ Given an array of size n, find the majority element. The majority element is the
 You may assume that the array is non-empty and the majority element always exist in the array.
 
 
-//Adobe, Zenefits
+Adobe, Zenefits
+*/
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
