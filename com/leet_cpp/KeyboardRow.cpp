3d20c39c095664ce6dcb1228d50435e1819ac4ea
mengarena <mengrufeng@gmail.com>
2019-08-05 16:44:14 -0700

    Update KeyboardRow.cpp

--- com/leet_cpp/KeyboardRow.cpp
+++ com/leet_cpp/KeyboardRow.cpp
@@ -1,9 +1,8 @@
 /*
-Given a List of words, return the words that can be typed using letters of alphabet on only one row's of American keyboard like the image below.
-
+Given a List of words, return the words that can be typed using letters of alphabet on only one row's of 
+American keyboard like the image below.
 
 American keyboard
-
 
 Example 1:
 Input: ["Hello", "Alaska", "Dad", "Peace"]
