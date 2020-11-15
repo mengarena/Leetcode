6924a36e7fdb279f22a17d25e696f082767b7245
mengarena <mengrufeng@gmail.com>
2020-11-15 15:39:22 -0800

    Update ContinuousSubarraySum.cpp

--- com/leet_cpp/ContinuousSubarraySum.cpp
+++ com/leet_cpp/ContinuousSubarraySum.cpp
@@ -7,12 +7,11 @@ write a function to check if the array has a continuous subarray of size at leas
 that is, sums up to n*k where n is also an integer.
 
 Example 1:
-
 Input: [23, 2, 4, 6, 7],  k=6
 Output: True
 Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
-Example 2:
 
+Example 2:
 Input: [23, 2, 6, 4, 7],  k=6
 Output: True
 Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and sums up to 42.
