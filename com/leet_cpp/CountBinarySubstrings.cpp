772572a40b930f59a6e4764bed311e3654803c3e
mengarena <mengrufeng@gmail.com>
2020-11-15 18:28:39 -0800

    Update CountBinarySubstrings.cpp

--- com/leet_cpp/CountBinarySubstrings.cpp
+++ com/leet_cpp/CountBinarySubstrings.cpp
@@ -14,8 +14,8 @@ Explanation: There are 6 substrings that have equal number of consecutive 1's an
 "0011", "01", "1100", "10", "0011", and "01".
 
 Notice that some of these substrings repeat and are counted the number of times they occur.
-
 Also, "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
+
 Example 2:
 Input: "10101"
 Output: 4
