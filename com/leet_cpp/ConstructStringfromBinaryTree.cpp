ccc101e62bfa9f92f004236985f8fd9dcd95c217
mengarena <mengrufeng@gmail.com>
2020-11-15 16:17:28 -0800

    Update ConstructStringfromBinaryTree.cpp

--- com/leet_cpp/ConstructStringfromBinaryTree.cpp
+++ com/leet_cpp/ConstructStringfromBinaryTree.cpp
@@ -4,7 +4,9 @@
 
 You need to construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way.
 
-The null node needs to be represented by empty parenthesis pair "()". And you need to omit all the empty parenthesis pairs that don't affect the one-to-one mapping relationship between the string and the original binary tree.
+The null node needs to be represented by empty parenthesis pair "()". 
+And you need to omit all the empty parenthesis pairs that don't affect the one-to-one mapping relationship 
+between the string and the original binary tree.
 
 Example 1:
 Input: Binary tree: [1,2,3,4]
@@ -19,6 +21,8 @@ Output: "1(2(4))(3)"
 Explanation: Originallay it needs to be "1(2(4)())(3()())", 
 but you need to omit all the unnecessary empty parenthesis pairs. 
 And it will be "1(2(4))(3)".
+
+
 Example 2:
 Input: Binary tree: [1,2,3,null,4]
        1
