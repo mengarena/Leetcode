747b14f61c407e68f5d37f5dc4cb4ce3a021a021
mengarena <mengrufeng@gmail.com>
2020-11-19 18:17:23 -0800

    Update FairCandySwap.cpp

--- com/leet_cpp/FairCandySwap.cpp
+++ com/leet_cpp/FairCandySwap.cpp
@@ -15,19 +15,18 @@ and ans[1] is the size of the candy bar that Bob must exchange.
 If there are multiple answers, you may return any one of them.  It is guaranteed an answer exists.
 
 Example 1:
-
 Input: A = [1,1], B = [2,2]
 Output: [1,2]
-Example 2:
 
+Example 2:
 Input: A = [1,2], B = [2,3]
 Output: [1,2]
-Example 3:
 
+Example 3:
 Input: A = [2], B = [1,3]
 Output: [2,3]
-Example 4:
 
+Example 4:
 Input: A = [1,2,5], B = [2,4]
 Output: [5,4]
  
