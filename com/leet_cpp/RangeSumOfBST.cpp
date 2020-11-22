fef36df4b08fb5d1133577ba12b703e526c5234d
mengarena <mengrufeng@gmail.com>
2020-11-22 14:01:19 -0800

    Update RangeSumOfBST.cpp

--- com/leet_cpp/RangeSumOfBST.cpp
+++ com/leet_cpp/RangeSumOfBST.cpp
@@ -6,7 +6,6 @@ Given the root node of a binary search tree, return the sum of values of all nod
 
 The binary search tree is guaranteed to have unique values.
 
-
 Example 1:
 Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
 Output: 32
@@ -16,15 +15,12 @@ Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
 Output: 23
  
 Note:
-
 The number of nodes in the tree is at most 10000.
 The final answer is guaranteed to be less than 2^31.
 
 Easy
-
 Google
 */
-
 
 /**
  * Definition for a binary tree node.
