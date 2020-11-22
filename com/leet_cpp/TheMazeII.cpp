dae71ed337ed9be60cdbaf5e79073dbf582cab50
mengarena <mengrufeng@gmail.com>
2020-11-22 01:11:12 -0800

    Update TheMazeII.cpp

--- com/leet_cpp/TheMazeII.cpp
+++ com/leet_cpp/TheMazeII.cpp
@@ -14,8 +14,7 @@ The maze is represented by a binary 2D array.
 You may assume that the borders of the maze are all walls. 
 The start and destination coordinates are represented by row and column indexes.
 
-Example 1
-
+Example 1:
 Input 1: a maze represented by a 2D array
 
 0 0 1 0 0
@@ -28,12 +27,10 @@ Input 2: start coordinate (rowStart, colStart) = (0, 4)
 Input 3: destination coordinate (rowDest, colDest) = (4, 4)
 
 Output: 12
-
 Explanation: One shortest way is : left -> down -> left -> down -> right -> down -> right.
              The total distance is 1 + 1 + 3 + 1 + 2 + 2 + 2 = 12.
 
-Example 2
-
+Example 2:
 Input 1: a maze represented by a 2D array
 
 0 0 1 0 0
@@ -46,7 +43,6 @@ Input 2: start coordinate (rowStart, colStart) = (0, 4)
 Input 3: destination coordinate (rowDest, colDest) = (3, 2)
 
 Output: -1
-
 Explanation: There is no way for the ball to stop at the destination.
 
 Note:
@@ -114,13 +110,9 @@ public:
                     dists[x][y] = dist + step;
                     q.push({x, y});
                 }
-                
             }
-            
         }
         
         return dists[destination[0]][destination[1]];
     }
 };
-
-
