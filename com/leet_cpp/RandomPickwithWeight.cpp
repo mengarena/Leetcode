7b143e8645a401026ce35ca71f6da2c663eb9d56
mengarena <mengrufeng@gmail.com>
2020-11-22 13:17:50 -0800

    Update RandomPickwithWeight.cpp

--- com/leet_cpp/RandomPickwithWeight.cpp
+++ com/leet_cpp/RandomPickwithWeight.cpp
@@ -6,18 +6,17 @@ Given an array w of positive integers, where w[i] describes the weight of index 
 write a function pickIndex which randomly picks an index in proportion to its weight.
 
 Note:
-
 1 <= w.length <= 10000
 1 <= w[i] <= 10^5
 pickIndex will be called at most 10000 times.
-Example 1:
 
+Example 1:
 Input: 
 ["Solution","pickIndex"]
 [[[1]],[]]
 Output: [null,0]
-Example 2:
 
+Example 2:
 Input: 
 ["Solution","pickIndex","pickIndex","pickIndex","pickIndex","pickIndex"]
 [[[1,3]],[],[],[],[],[]]
@@ -28,11 +27,9 @@ The input is two lists: the subroutines called and their arguments.
 Solution's constructor has one argument, the array w. pickIndex has no arguments. 
 Arguments are always wrapped with a list, even if there aren't any.
 
-
 Medium:
 Uber
 */
-
 
 class Solution {
 public:
