8f5b7a6121a25cc11203fbb314c42b5f66b45244
mengarena <mengrufeng@gmail.com>
2020-11-26 21:31:34 -0800

    Update MostCommonWord.cpp

--- com/leet_cpp/MostCommonWord.cpp
+++ com/leet_cpp/MostCommonWord.cpp
@@ -32,7 +32,6 @@ paragraph only consists of letters, spaces, or the punctuation symbols !?',;.
 There are no hyphens or hyphenated words.
 Words only consist of letters, never apostrophes or other punctuation symbols.
 
-
 Easy
 */
 
