36d53e76f489366307f7f06f7535bc804faefdc4
mengarena <mengrufeng@gmail.com>
2020-11-27 12:57:58 -0800

    Update SingleElementInSortedArray.cpp

--- com/leet_cpp/SingleElementInSortedArray.cpp
+++ com/leet_cpp/SingleElementInSortedArray.cpp
@@ -7,12 +7,10 @@ except for one element which appears exactly once.
 Find this single element that appears only once.
 
 Example 1:
-
 Input: [1,1,2,3,3,4,4,8,8]
 Output: 2
 
 Example 2:
-
 Input: [3,3,7,7,10,11,11]
 Output: 10
 
@@ -22,7 +20,6 @@ Medium
 
 Amazon, Google, Facebook, Apple
 */
-
 
 class Solution {
 public:
