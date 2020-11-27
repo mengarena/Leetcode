73751de9558666ef6d85586947aabe535eb3e07b
mengarena <mengrufeng@gmail.com>
2020-11-26 21:41:32 -0800

    Update MostStonesRemovedWithSameRowColumn.cpp

--- com/leet_cpp/MostStonesRemovedWithSameRowColumn.cpp
+++ com/leet_cpp/MostStonesRemovedWithSameRowColumn.cpp
@@ -9,31 +9,24 @@ Now, a move consists of removing a stone that shares a column or row with anothe
 
 What is the largest possible number of moves we can make?
 
- 
-
 Example 1:
-
 Input: stones = [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]
 Output: 5
-Example 2:
 
+Example 2:
 Input: stones = [[0,0],[0,2],[1,1],[2,0],[2,2]]
 Output: 3
-Example 3:
 
+Example 3:
 Input: stones = [[0,0]]
 Output: 0
  
-
 Note:
-
 1 <= stones.length <= 1000
 0 <= stones[i][j] < 10000
 
-
 Medium - Hard
 */
-
 
 class Solution {
 public:
