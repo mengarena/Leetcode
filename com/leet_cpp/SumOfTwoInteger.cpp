0ce2e9a3c5a4e7d9c27ae06c4228f3ba42807f81
mengarena <mengrufeng@gmail.com>
2020-11-27 13:27:41 -0800

    Update SumOfTwoInteger.cpp

--- com/leet_cpp/SumOfTwoInteger.cpp
+++ com/leet_cpp/SumOfTwoInteger.cpp
@@ -1,13 +1,16 @@
-//371. Sum of Two Integers   QuestionEditorial Solution  My Submissions
+/*
+371. Sum of Two Integers
 
-//Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
 
-//Example:
-//Given a = 1 and b = 2, return 3.
+Example:
+Given a = 1 and b = 2, return 3.
 
 Hulu
 
-//Easy
+Easy
+*/
+
 class Solution {
 public:
     int getSum(int a, int b) {
