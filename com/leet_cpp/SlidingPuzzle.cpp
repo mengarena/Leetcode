56372cb62ff757bde24c9599a87bc281dc8b1e10
mengarena <mengrufeng@gmail.com>
2020-11-28 11:27:03 -0800

    Update SlidingPuzzle.cpp

--- com/leet_cpp/SlidingPuzzle.cpp
+++ com/leet_cpp/SlidingPuzzle.cpp
@@ -12,16 +12,18 @@ Given a puzzle board, return the least number of moves required so that the stat
 If it is impossible for the state of the board to be solved, return -1.
 
 Examples:
-
 Input: board = [[1,2,3],[4,0,5]]
 Output: 1
 Explanation: Swap the 0 and the 5 in one move.
+
 Input: board = [[1,2,3],[5,4,0]]
 Output: -1
 Explanation: No number of moves will make the board solved.
+
 Input: board = [[4,1,2],[5,0,3]]
 Output: 5
 Explanation: 5 is the smallest number of moves that solves the board.
+
 An example path:
 After move 0: [[4,1,2],[5,0,3]]
 After move 1: [[4,1,2],[0,5,3]]
@@ -31,15 +33,13 @@ After move 4: [[1,2,0],[4,5,3]]
 After move 5: [[1,2,3],[4,5,0]]
 Input: board = [[3,2,4],[1,5,0]]
 Output: 14
-Note:
 
+Note:
 board will be a 2 x 3 array as described above.
 board[i][j] will be a permutation of [0, 1, 2, 3, 4, 5].
 
-
 Hard
 */
-
 
 class Solution {
 public:
