e3219d1482862fcdbe971a8e5cb8af338e15782a
mengarena <mengrufeng@gmail.com>
2020-12-10 21:00:50 -0800

    Update CombinationSumIII.java

--- com/leet/CombinationSumIII.java
+++ com/leet/CombinationSumIII.java
@@ -3,36 +3,26 @@ package com.leet;
 import java.util.ArrayList;
 import java.util.List;
 
-
 //Find all possible combinations of k numbers that add up to a number n, 
 //given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
 //
 //Ensure that numbers within the set are sorted in ascending order.
 //
-//
 //Example 1:
-//
 //Input: k = 3, n = 7
-//
 //Output:
-//
 //[[1,2,4]]
 //
 //Example 2:
-//
 //Input: k = 3, n = 9
-//
 //Output:
-//
 //[[1,2,6], [1,3,5], [2,3,4]]
 		
-
 public class CombinationSumIII {
 
 	public CombinationSumIII() {
 		// TODO Auto-generated constructor stub
 	}
-
 
 	public void run() {
 		int k = 2;
@@ -47,8 +37,7 @@ public class CombinationSumIII {
 			System.out.println();
 		}
 	}
-	
-	
+
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> ret = new ArrayList<List<Integer>>();
         if (k <= 0 || n <= 0 || k > n) return ret;
