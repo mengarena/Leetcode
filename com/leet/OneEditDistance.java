1aeeaa0d16c8304b9328d5ef4e3c4478487f250c
mengarena <mengrufeng@gmail.com>
2020-12-18 17:30:08 -0800

    Update OneEditDistance.java

--- com/leet/OneEditDistance.java
+++ com/leet/OneEditDistance.java
@@ -19,10 +19,10 @@ public class OneEditDistance {
 	    	if (s.charAt(i) != t.charAt(i)) {
 	    		if (s.length() == t.length()) // s has the same length as t, so the only possibility is replacing one char in s and t
 	    			return s.substring(i + 1).equals(t.substring(i + 1));
-				else if (s.length() < t.length()) // t is longer than s, so the only possibility is deleting one char from t
-					return s.substring(i).equals(t.substring(i + 1));	        	
-				else // s is longer than t, so the only possibility is deleting one char from s
-					return t.substring(i).equals(s.substring(i + 1));
+			else if (s.length() < t.length()) // t is longer than s, so the only possibility is deleting one char from t
+				return s.substring(i).equals(t.substring(i + 1));	        	
+			else // s is longer than t, so the only possibility is deleting one char from s
+				return t.substring(i).equals(s.substring(i + 1));
 	    	}
 	    } 
 	    
