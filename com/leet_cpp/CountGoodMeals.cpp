f4de99587bc1bc835fb7cfd6a2394701a4b584c9
mengarena <mengrufeng@gmail.com>
2021-01-09 23:34:13 -0800

    Update CountGoodMeals.cpp

--- com/leet_cpp/CountGoodMeals.cpp
+++ com/leet_cpp/CountGoodMeals.cpp
@@ -7,7 +7,7 @@ A good meal is a meal that contains exactly two different food items with a sum 
 You can pick any two different foods to make a good meal.
 
 Given an array of integers deliciousness where deliciousness[i] is the deliciousness of the ith item of food, 
-return the number of different good meals you can make from this list modulo 109 + 7.
+return the number of different good meals you can make from this list modulo 10^9 + 7.
 
 Note that items with different indices are considered different even if they have the same deliciousness value.
 
@@ -23,8 +23,8 @@ Output: 15
 Explanation: The good meals are (1,1) with 3 ways, (1,3) with 9 ways, and (1,7) with 3 ways.
  
 Constraints:
-1 <= deliciousness.length <= 105
-0 <= deliciousness[i] <= 220
+1 <= deliciousness.length <= 10^5
+0 <= deliciousness[i] <= 2^20
 
 Medium
 */
